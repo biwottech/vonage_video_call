4e3b330fcc5481c265ba243bc81ccae8f15ba216
Evans Koech <biwottech@gmail.com>
2023-07-29 08:57:58 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const { Vonage } = require('@vonage/server-sdk');
 const WebSocket = require('ws');
 const cors = require('cors');
-const from = process.env.VONAGE_NUMBER;
+const VONAGE_NUMBER = process.env.VONAGE_NUMBER;
 const USER_PHONE_NUMBER = process.env.TO_NUMBER;
-const answer_url = process.env.ANSWER_WEBHOOK_URL
-const event_url = process.env.WEBHOOK_URL
+const ANSWER_URL = process.env.ANSWER_WEBHOOK_URL
+const EVENT_URL = process.env.WEBHOOK_URL
 const input_url = process.env.OUTPUT_WEBHOOK_URL;
 const PORT = process.env.PORT || 5000;
 
@@ -19,13 +19,76 @@ const vonage = new Vonage({
     apiSecret: process.env.VONAGE_API_SECRET,
     applicationId: process.env.VONAGE_APPLICATION_ID,
     privateKey: process.env.VONAGE_PRIVATE_KEY_PATH,
-});
+}, { debug: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+// function to instantiate a call
+async function makeCall(to) {
+    vonage.voice.createOutboundCall(
+        {
+            to: [{ type: 'phone', number: VONAGE_NUMBER }],
+            from: { type: 'phone', number: to },
+            answer_url: [ANSWER_URL],
+            event_url: [EVENT_URL],
+            machine_detection: 'continue'
+        },
+        (err, response) => {
+            if (err) {
+                console.error('Error making outbound call:', err);
+            } else {
+                console.log('Outbound call response:', response);
+            }
+        }
+    );
+}
+
+// mute a call
+const muteCall = (UUID) => {
+    vonage.voice.muteCall(UUID)
+        .then(resp => console.log(resp))
+        .catch(err => console.error(err));
+}
+
+// unmute a call
+const unMuteCall = (UUID) => {
+    vonage.voice.unmuteCall(UUID)
+        .then(resp => console.log(resp))
+        .catch(err => console.error(err));
+}
+
+// Track NCCO progress
+// In this code snippet you see how to track how far through an NCCO a caller gets using the notify action
+const onNotification = (request, response) => {
+    const ncco = [{
+        "action": "talk",
+        "text": "Your notification has been received, loud and clear"
+    }]
+
+    response.json(ncco)
+}
+
+
+// record event logs to access the logs of what happended to the call
+const record_event_logs = (event_logs) => {
+    console.log(event_logs);
+}
+
+function uploadFile(localPath, fileName) {
+    fs.readFile(localPath, (err, data) => {
+        if (err) { throw err }
+        console.log(`${fileName} uploaded to recordings folder done bucket`)
+    });
+}
+
+
+
 // Start the server
+// holds a record of each status that a particular phone call or "conversation" went through
+//It is important when we shall start many outgoing calls at one time
 var server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
@@ -34,19 +97,77 @@ var server = app.listen(PORT, () => {
 const wss = new WebSocket.Server({ server });
 
 
-// Endpoint to handle Vonage answer webhook
-app.post('/answer', (req, res) => {
-    // Your answer logic here
-    // Return a valid NCCO (Nexmo Call Control Object) response to define call flow
-    const ncco = [
-        {
-            action: 'talk',
-            text: 'Hello! This is an outbound call using Vonage Voice API with WebSockets.',
-        },
-    ];
-   return  res.json(ncco);
+app.post('/call', (req, res) => {
+    let contacts = {};
+    contacts.forEach(element => {
+        makeCall()
+        // ensure you slee(pause) to avoid hitting the API rate limit.
+    });
 });
 
+
+app.post('/event', (req, res) => {
+    let status = req.body.status;
+    let conversation_uuid = req.body.conversation_uuid;
+
+    switch (status) {
+        case 'ringing':
+            record_event_logs({ UUID: `${conversation_uuid} - ringing.` });
+            break;
+        case 'answered':
+            record_event_logs({ UUID: `${conversation_uuid} - ringing.` });
+            break;
+        case 'complete':
+            record_event_logs({ UUID: `${conversation_uuid} - ringing.` });
+            break;
+        default:
+            break;
+    }
+    return res.status(204).send("");
+})
+
+// Endpoint to handle Vonage answer webhook
+app.post('/answer', onAnswer).post('/webhooks/recordings', onRecording);
+
+
+// onAnswer
+const onAnswer = (req, res) => {
+    return res.json([{
+        action: 'talk',
+        text: 'Thanks for calling, we will connect you now'
+    },
+    {
+        action: 'connect',
+        endpoint: [{
+            type: 'phone',
+            number: process.env.OTHER_PHONE_NUMBER
+        }]
+    },
+    {
+        action: 'record',
+        eventUrl: [`${req.protocol}://${req.get('host')}/webhooks/recording`],
+        split: 'conversation',
+        channels: 2,
+        format: 'mp3'
+    }
+    ]);
+}
+
+// recordning function
+const onRecording = () => {
+    let audioFileName = `vonage-${shortid.generate()}.mp3`
+    let audioFileLocalPath = `./recordings/${audioFileName}`
+    nexmo.files.save(req.body.recording_url, audioFileLocalPath, (err, res) => {
+        if (err) {
+            console.log("Could not save audio file")
+            console.error(err)
+        }
+        else {
+            uploadFile(audioFileLocalPath, audioFileName)
+        }
+    });
+    return res.status(204).send("");
+}
 
 // WebSocket connection handling
 wss.on('connection', (ws) => {
@@ -55,29 +176,10 @@ wss.on('connection', (ws) => {
         console.log('Received message:', message);
         // Parse the incoming message
         const data = JSON.parse(message);
-        const ANSWER_URL = 'https://raw.githubusercontent.com/nexmo-community/ncco-examples/gh-pages/text-to-speech.json'
-
-        // Make an outbound call when a specific message is received
         if (data.type === 'makeCall') {
-            const destinationNumber = data.destinationNumber;
-            const callerId = data.callerId;
-            vonage.voice.createOutboundCall(
-                {
-                    to: [{ type: 'phone', number: destinationNumber }],
-                    from: { type: 'phone', number: callerId },
-                    answer_url: [ANSWER_URL], // Replace with your own server URL
-                },
-                (err, response) => {
-                    if (err) {
-                        console.error('Error making outbound call:', err);
-                    } else {
-                        console.log('Outbound call response:', response);
-                    }
-                }
-            );
+            makeCall()
         }
     });
-
     ws.on('close', () => {
         console.log('Client disconnected');
     });
---
+++ server/ncco.js
@@ -0,0 +1,137 @@
+const record_ncco = () => {
+    // The record action is asynchronous.
+    //  Recording starts when the record action is executed in the NCCO and finishes when the synchronous condition in the action is met.
+    // That is, endOnSilence, timeOut or endOnKey. If you do not set a synchronous condition,
+    //  the Voice API immediately executes the next NCCO without recording.
+    return [
+        {
+            "action": "record",
+            "eventUrl": ["https://example.com/recordings"]
+        },
+        {
+            "action": "connect",
+            "eventUrl": ["https://example.com/events"],
+            "from": "447700900000",
+            "endpoint": [
+                {
+                    "type": "phone",
+                    "number": "447700900001"
+                }``
+            ]
+        }
+    ];
+}
+
+
+const connect_ncco = () => {
+    // You can use the connect action to connect a call to endpoints such as phone numbers or a VBC extension.
+    // This action is synchronous, after a connect the next action in the NCCO stack is processed.
+    //  A connect action ends when the endpoint you are calling is busy or unavailable.
+    // You ring endpoints sequentially by nesting connect actions.
+
+}
+
+const connect_fall_back_ncco = () => {
+    // You can provide a fallback for Calls that do not connect. To do this set the eventType to synchronous and return an NCCO from the eventUrl if the Call enters any of the following states:
+    // timeout - your user did not answer your call with ringing_timer seconds
+    // failed - the call failed to complete
+    // rejected - the call was rejected
+    // unanswered - the call was not answered
+    // busy - the person being called was on another call
+
+    [
+        {
+            "action": "connect",
+            "from": "447700900000",
+            "timeout": 5,
+            "eventType": "synchronous",
+            "eventUrl": [
+                "https://example.com/event-fallback"
+            ],
+            "endpoint": [
+                {
+                    "type": "phone",
+                    "number": "447700900001"
+                }
+            ]
+        }
+    ]
+
+}
+
+const connect_phone_ncco = () => {
+    [
+        {
+            "action": "talk",
+            "text": "Please wait while we connect you"
+        },
+        {
+            "action": "connect",
+            "eventUrl": ["https://example.com/events"],
+            "timeout": "45",
+            "from": "447700900000",
+            "endpoint": [
+                {
+                    "type": "phone",
+                    "number": "447700900001",
+                    "dtmfAnswer": "2p02p"
+                }
+            ]
+        }
+    ]
+}
+
+const connect_recorded_proxxy = () => {
+    [
+        {
+            "action": "record",
+            "eventUrl": ["https://example.com/recordings"]
+        },
+        {
+            "action": "connect",
+            "eventUrl": ["https://example.com/events"],
+            "from": "447700900000",
+            "endpoint": [
+                {
+                    "type": "phone",
+                    "number": "447700900001"
+                }
+            ]
+        }
+    ]
+}
+
+const connect_websocket_end_point = () => {
+    [
+        {
+            "action": "talk",
+            "text": "Please wait while we connect you"
+        },
+        {
+            "action": "connect",
+            "eventType": "synchronous",
+            "eventUrl": [
+                "https://example.com/events"
+            ],
+            "from": "447700900000",
+            "endpoint": [
+                {
+                    "type": "websocket",
+                    "uri": "ws://example.com/socket",
+                    "content-type": "audio/l16;rate=16000",
+                    "headers": {
+                        "name": "J Doe",
+                        "age": 40,
+                        "address": {
+                            "line_1": "Apartment 14",
+                            "line_2": "123 Example Street",
+                            "city": "New York City"
+                        },
+                        "system_roles": [183493, 1038492, 22],
+                        "enable_auditing": false
+                    }
+                }
+            ]
+        }
+    ]
+}
